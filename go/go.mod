1a43a1c4aea148f7523784a250f25a3f8e9ca6a6
Dart Protocol <hotbeatdropper@gmail.com>
2026-09-08 18:37:52 +0200

    Website

--- go/go.mod
+++
@@ -1,5 +0,0 @@
-module dartgo
-
-go 1.15
-
-require github.com/gorilla/websocket v1.5.3
--- src/demo.ts
+++
@@ -1,9 +0,0 @@
-import { DartGroupServer } from './server';
-
-console.log("Starting Dart Web Demo Server...");
-const server = new DartGroupServer(9000);
-
-console.log("UDP Server listening on 9000");
-console.log("TCP Server listening on 9001");
-console.log("HTTP / WebSocket Server listening on 9002");
-console.log("\nOpen http://localhost:9002 in two browser tabs to chat!");
